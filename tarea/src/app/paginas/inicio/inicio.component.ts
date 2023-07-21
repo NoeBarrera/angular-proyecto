af1d9f13aff1cd28b2dd1226d774650701a996e2
Your Name <you@example.com>
2023-07-21 15:11:30 -0400

    estilos y personajes

--- tarea/src/app/paginas/inicio/inicio.component.ts
+++ tarea/src/app/paginas/inicio/inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ServicioInicioService } from 'src/app/servicios/servicio-inicio.service';
 
 @Component({
@@ -7,12 +7,35 @@ import { ServicioInicioService } from 'src/app/servicios/servicio-inicio.service
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-  datos:any;
-  constructor (private servicio:ServicioInicioService){}
+  datos: any;
+  constructor(private servicio: ServicioInicioService) { }
+
   ngOnInit(): void {
-
     this.datos = this.servicio.getCharacters();
+  }
 
+  mostrarPersonajes(id: string): void {
+    var personajes = document.getElementById(id);
+    if (personajes) {
+      var casa = document.querySelector(`img[src$='${id}.png']`);
+      var modal = document.createElement('div');
+      modal.className = 'modal';
+      var modalContent = document.createElement('div');
+      modalContent.className = 'modal-content';
+      modalContent.innerHTML = personajes.innerHTML;
+      modal.appendChild(modalContent);
+      document.body.appendChild(modal);
+
+      modal.style.display = 'block';
+
+      window.onclick = function (event) {
+        if (event.target == modal) {
+          modal.style.display = 'none';
+          document.body.removeChild(modal);
+        }
+      }
+    }
   }
 
 }
+
